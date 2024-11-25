43c25d970ec0c813ecc3037c7f68ebeeb5d038c9
ShadowAlzazel <pastenalfredojr@gmail.com>
2024-11-24 22:17:24 -0800

    New Async methods for chat models

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,15 +1,25 @@
 import asyncio
-
 from javascript import require, On
+
+# Modules
+from ..models import model_manager
+
 mineflayer = require('mineflayer')
 pathfinder = require('mineflayer-pathfinder')
 
 class Agent:
 
-    def __init__(self, name: str="gpt"):
-        self.name = name
+    # Initialize API and model
+    def __init__(self, **profile):
+        self.name = profile["name"]
+        self.api = profile["api"]
+        # Get then init new model
+        new_model = model_manager.find_model(api=profile["api"], model=profile["model"])
+        model_args = model_manager.get_model_args(**profile)
+        self.model = new_model(**model_args)
+        # Conversing
 
-
+    # Start Bot in Minecraft
     def start(self, **kwargs):
         bot = mineflayer.createBot({
             "host": kwargs["host"],
@@ -18,3 +28,14 @@ class Agent:
             "version": kwargs["version"],
             "username": self.name
         })
+        
+        
+    # Chat
+    async def send_chat(self, user: str, message: str):
+        new_message = {
+            "role": "user",
+            "content": f'{user}: {message}'
+        }
+        
+        response = await self.model.send_request(messages=[new_message])
+        print(response)
